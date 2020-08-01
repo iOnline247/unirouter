2f70dba7c68130bb77d1da80ed3b843adcc14710
Matthew B <515152+iOnline247@users.noreply.github.com>
2020-08-01 12:02:52 -0400

    Fix scenario responses

--- src/routes/myProject.js
+++ src/routes/myProject.js
@@ -1,6 +1,19 @@
 const myProject = {
   scenario1: {
-    responses: [1, 2, 3, 4, 5, 6],
+    responses: [
+      {
+        status: 200,
+        response: { test: true },
+      },
+      {
+        status: 201,
+        response: { test: true, here: [1, 2, 3] },
+      },
+      {
+        status: 400,
+        response: { msg: "Invalid Request" },
+      },
+    ],
   },
 };
 
