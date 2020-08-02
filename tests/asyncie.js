cb06e5c130cefd2de4808439e7e9b9b202e87736
Matthew B <515152+iOnline247@users.noreply.github.com>
2020-08-02 02:56:29 -0400

    Initial commit

---
+++ tests/asyncie.js
@@ -0,0 +1,24 @@
+// TODO:
+// install fetch for node.
+
+const getData = async () => {
+  let output;
+
+  try {
+    const res = await fetch(`/whack/${Math.floor(Math.random() * 100)}`, {
+      method: "GET",
+    });
+
+    output = await res.text();
+  } catch (err) {
+    console.error(err);
+  }
+
+  return output;
+};
+
+(async function () {
+  const promisees = [getData(), getData(), getData()];
+
+  await Promise.all(promisees);
+})();
