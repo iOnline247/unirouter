7a832b390710c59983c68a8f7b28e73540120e33
Matthew B <515152+iOnline247@users.noreply.github.com>
2020-08-02 14:46:22 -0400

    Implemented req.logger. WIP

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-console */
 import chalk from "chalk";
-import { v4 as uuidv4 } from "uuid";
 
 const LOG_TYPES = {
   DEBUG: "DEBUG",
@@ -16,47 +15,50 @@ function formatMsg(...[id, logLevel, msg]) {
 }
 
 class Logger {
-  #req;
+  // #req;
 
-  #correlationId;
+  correlationId;
 
-  get request() {
-    return this.#req;
+  constructor(id) {
+    this.correlationId = id;
   }
+  // get request() {
+  //   return this.#req;
+  // }
 
-  set request(req) {
-    this.#req = req;
-  }
+  // set request(req) {
+  //   this.#req = req;
+  // }
 
   debug(msg) {
     const logLevel = LOG_TYPES.DEBUG;
 
-    console.debug(formatMsg(this.#correlationId, logLevel, msg));
+    console.debug(formatMsg(this.correlationId, logLevel, msg));
   }
 
   error(msg) {
     const logLevel = LOG_TYPES.ERROR;
 
-    console.error(formatMsg(this.#correlationId, logLevel, msg));
+    console.error(formatMsg(this.correlationId, logLevel, msg));
   }
 
   info(msg) {
     const logLevel = LOG_TYPES.INFO;
 
-    console.info(formatMsg(this.#correlationId, logLevel, msg));
+    console.info(formatMsg(this.correlationId, logLevel, msg));
   }
 
   log(msg) {
     const logLevel = LOG_TYPES.LOG;
 
-    console.log(formatMsg(this.#correlationId, logLevel, msg));
+    console.log(formatMsg(this.correlationId, logLevel, msg));
   }
 
   warn(msg) {
     const logLevel = LOG_TYPES.WARN;
 
-    console.warn(formatMsg(this.#correlationId, logLevel, msg));
+    console.warn(formatMsg(this.correlationId, logLevel, msg));
   }
 }
 
-export default { Logger };
+export default Logger;
