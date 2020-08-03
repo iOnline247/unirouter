e4b86ae7657b7d7a5b32192862b577e91f092542
Matthew B <515152+iOnline247@users.noreply.github.com>
2020-08-03 01:07:04 -0400

    Case insensitive config values that map to responses

--- src/utils/common.js
+++ src/utils/common.js
@@ -2,5 +2,12 @@ import { promisify } from "util";
 
 const noop = () => {};
 const sleep = promisify(setTimeout);
+const getValueByKey = (prop, obj) => {
+  const key = Object.keys(obj || {}).find(
+    (v) => v.toUpperCase() === prop.toUpperCase()
+  );
 
-export { noop, sleep };
+  return obj[key];
+};
+
+export { getValueByKey, noop, sleep };
