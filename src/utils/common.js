41fa9cacb27edeba9ca5d6b6cd9dcbfd97c96dd9
Matthew B <515152+iOnline247@users.noreply.github.com>
2020-08-03 01:35:00 -0400

    Cleanup

--- src/utils/common.js
+++ src/utils/common.js
@@ -4,7 +4,7 @@ const noop = () => {};
 const sleep = promisify(setTimeout);
 const getValueByKey = (prop, obj) => {
   const key = Object.keys(obj || {}).find(
-    (v) => v.toUpperCase() === prop.toUpperCase()
+    (v) => v.toUpperCase() === `${prop}`.toUpperCase()
   );
 
   return obj[key];
